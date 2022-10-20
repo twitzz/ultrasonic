41ac456180f9bad9e460a95a1a19d941df9e6a34
twitzz <116275213+twitzz@users.noreply.github.com>
2022-10-20 23:00:06 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,14 +6,17 @@ function car_backward () {
 }
 function LED_L () {
     mecanumRobot.setLed(LedCount.Left, LedState.ON)
-    basic.pause(1000)
-    mecanumRobot.setLed(LedCount.Left, LedState.OFF)
+    mecanumRobot.setLed(LedCount.Right, LedState.OFF)
 }
 function car_left () {
     mecanumRobot.Motor(LR.Upper_left, MD.Back, 40)
     mecanumRobot.Motor(LR.Lower_left, MD.Back, 40)
     mecanumRobot.Motor(LR.Upper_right, MD.Forward, 40)
     mecanumRobot.Motor(LR.Lower_right, MD.Forward, 40)
+}
+function LED_L2 () {
+    mecanumRobot.setLed(LedCount.Left, LedState.OFF)
+    mecanumRobot.setLed(LedCount.Right, LedState.OFF)
 }
 function car_forward () {
     mecanumRobot.Motor(LR.Upper_left, MD.Forward, 40)
@@ -22,8 +25,11 @@ function car_forward () {
     mecanumRobot.Motor(LR.Lower_right, MD.Forward, 40)
 }
 function LED_R () {
+    mecanumRobot.setLed(LedCount.Left, LedState.OFF)
     mecanumRobot.setLed(LedCount.Right, LedState.ON)
-    basic.pause(1000)
+}
+function LED_R2 () {
+    mecanumRobot.setLed(LedCount.Left, LedState.OFF)
     mecanumRobot.setLed(LedCount.Right, LedState.OFF)
 }
 function car_right () {
@@ -37,11 +43,13 @@ let distance_right = 0
 let distance = 0
 basic.showIcon(IconNames.Happy)
 mecanumRobot.setServo(80)
+mecanumRobot.setLed(LedCount.Left, LedState.ON)
+mecanumRobot.setLed(LedCount.Right, LedState.ON)
 basic.forever(function () {
     distance = mecanumRobot.ultra()
     if (distance < 20) {
         car_backward()
-        basic.pause(200)
+        basic.pause(300)
         mecanumRobot.state(MotorState.stop)
         basic.pause(500)
         mecanumRobot.setServo(0)
@@ -56,12 +64,14 @@ basic.forever(function () {
             car_right()
             mecanumRobot.setServo(80)
             LED_R()
-            basic.pause(500)
+            basic.pause(600)
+            LED_R2()
         } else {
             car_left()
             mecanumRobot.setServo(80)
             LED_L()
-            basic.pause(500)
+            basic.pause(600)
+            LED_L2()
         }
     } else {
         car_forward()
